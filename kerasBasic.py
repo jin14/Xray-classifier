abd45ecd09263954660dc807cb6bd5136d30feef
jin14 <jinwei9182@gmail.com>
2018-10-13 23:33:56 +0800

    fix plotting

--- kerasBasic.py
+++ kerasBasic.py
@@ -1,23 +1,35 @@
 import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
+import time
 from sklearn.model_selection import StratifiedKFold, KFold
-from keras.layers import Input, Flatten, Dense
+from keras.layers import Input, Flatten, Dense, Dropout
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model
 from keras.applications import vgg16, resnet50
 from keras import optimizers
-# tensor-gpu 1.9.0
 
-def createVGG(dim, weights, lr):
-
+def createVGG(dim, weights, lr, train_last_layer=False):
     input = Input(shape=(dim, dim, 3), name="image_input")
     vgg = vgg16.VGG16(include_top=False, weights=weights)
+
+    if train_last_layer:
+        set_trainable = False
+        for layers in vgg.layers:
+            if layers.name == "block5_conv1":
+                set_trainable = True
+            if set_trainable:
+                layers.trainable = True
+            else:
+                layers.trainable = False
+
     output_vgg = vgg(input)
 
     out = Flatten(name='flatten')(output_vgg)
-    out = Dense(4096, activation='relu', name='fc1')(out)
-    out = Dense(4096, activation='relu', name='fc2')(out)
+    out = Dense(2048, activation='relu', name='fc1')(out)
+    out = Dropout(0.7)(out)
+    out = Dense(2048, activation='relu', name='fc2')(out)
+    out = Dropout(0.7)(out)
     out = Dense(15, activation='softmax', name='predictions')(out)
 
     vgg_modified = Model(input=input, output=out)
@@ -25,16 +37,13 @@ def createVGG(dim, weights, lr):
 
     return vgg_modified
 
-def generatePlot(model, dir, name ,lr):
 
-    acc = model.history['acc']
-    val_acc = model.history['val_acc']
-    loss = model.history['loss']
-    val_loss = model.history['val_loss']
+def generatePlot(acc, val_acc, loss, val_loss, dir, name, lr):
+
     epochs = range(1, len(acc) + 1)
 
     plt.figure()
-    plt.plot(epochs, acc , 'b*-', label="Training Accuracy")
+    plt.plot(epochs, acc, 'b*-', label="Training Accuracy")
     plt.plot(epochs, val_acc, 'r*-', label="Validation Accuracy")
     plt.title("Training and Validation Accuracy")
     plt.xlabel("Epochs")
@@ -43,7 +52,7 @@ def generatePlot(model, dir, name ,lr):
     plt.savefig(dir + name + '_acc_{}.png'.format(lr))
 
     plt.figure()
-    plt.plot(epochs, loss , 'bo-', label="Training Loss")
+    plt.plot(epochs, loss, 'bo-', label="Training Loss")
     plt.plot(epochs, val_loss, 'ro-', label="Validation Loss")
     plt.title("Training and Validation Loss")
     plt.xlabel("Epochs")
@@ -62,6 +71,7 @@ if __name__ == "__main__":
     parser.add_argument('--d', type=int, help="image width or length, both should be the same")
     parser.add_argument('--k', type=int, help="K fold cross validation")
     parser.add_argument('--e', type=int, help="No of epochs")
+    parser.add_argument('--tr',type=int, help="Is last layer trainable: 0 - false, 1 True")
 
     args = parser.parse_args()
 
@@ -69,7 +79,7 @@ if __name__ == "__main__":
     WEIGHTS = args.w if args.w == 'imagenet' else None
     DIM = args.d
     EPOCHS = args.e
-    #learning_rates = [0.001, 0.01, 0.0001, 0.00001]
+    # learning_rates = [0.001, 0.01, 0.0001, 0.00001]
     learning_rates = [0.0001]
 
     df = pd.read_csv(args.t + args.lt)
@@ -77,16 +87,21 @@ if __name__ == "__main__":
     kfold = KFold(n_splits=args.k, shuffle=True)
 
     train_datagen = ImageDataGenerator(
-        rescale=1./255,
+        rescale=1. / 255,
     )
     valid_datagen = ImageDataGenerator(
-        rescale=1./255
+        rescale=1. / 255
     )
 
     for lr in learning_rates:
         print("Learning rate:", lr)
-        model = createVGG(DIM, WEIGHTS, lr)
-        print("Model Summary: ", model.summary())
+        model = createVGG(DIM, WEIGHTS, lr, bool(args.tr))
+        model.summary()
+
+        acc = []
+        val_acc = []
+        loss = []
+        val_loss = []
 
         for train_index, test_index in kfold.split(img_indexes):
             traindf = df.iloc[train_index, :].reset_index()
@@ -98,7 +113,7 @@ if __name__ == "__main__":
                                                                 y_col="label",
                                                                 has_ext=True,
                                                                 class_mode="categorical",
-                                                                target_size=(DIM,DIM),
+                                                                target_size=(DIM, DIM),
                                                                 batch_size=BATCH_SIZE)
 
             valid_generator = valid_datagen.flow_from_dataframe(dataframe=validdf,
@@ -107,16 +122,25 @@ if __name__ == "__main__":
                                                                 y_col="label",
                                                                 has_ext=True,
                                                                 class_mode="categorical",
-                                                                target_size=(DIM,DIM),
+                                                                target_size=(DIM, DIM),
                                                                 batch_size=BATCH_SIZE)
 
             result = model.fit_generator(
                 generator=train_generator,
-                steps_per_epoch=len(traindf.index)/BATCH_SIZE,
+                steps_per_epoch=len(traindf.index) / BATCH_SIZE,
                 epochs=EPOCHS,
                 validation_data=valid_generator,
-                validation_steps=len(validdf.index)/BATCH_SIZE
+                validation_steps=len(validdf.index) / BATCH_SIZE,
+                workers=32,
+                use_multiprocessing=True
             )
 
-            model.save('model_{}'.format(lr))
-            generatePlot(result, args.p, 'vgg16_' + str(BATCH_SIZE) + '_' + str(DIM, lr))
+            model.save('model_edge_{}_{}_{}'.format(lr, BATCH_SIZE, DIM))
+
+            acc.extend(result.history['acc'])
+            val_acc.extend(result.history['val_acc'])
+            loss.extend(result.history['loss'])
+            val_loss.extend(result.history['val_loss'])
+
+
+        generatePlot(acc, val_acc, loss, val_loss, args.p, 'vgg16_' + str(BATCH_SIZE) + '_' + str(DIM) + '_' + str(time.time()), str(lr))
